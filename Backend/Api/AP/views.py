9ede8c3f3fd9bd1177a5a62869bccba6b66e9f10
Kwakuafram <vidaforkuo557@gmail.com>
2022-10-30 17:05:28 -1200

    GET

--- Backend/Api/AP/views.py
+++ Backend/Api/AP/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
 
 # Create your views here.
+def home(request):
+    return JsonResponse( { "slackUsername": "Richmond Forkuo Afram", "backend": "True", "age": "25", "bio": "i have the grit of becoming a backend developer" })
